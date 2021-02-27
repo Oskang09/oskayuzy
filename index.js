a0194e11700854288ee28ec596e0b8beeb819e86
Oskang09 <developer.oskang@gmail.com>
2021-02-27 13:05:59 +0800

    fix: api agent

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { dig } = require('digdata');
 const { create } = require('apisauce');
 const qs = require('query-string');
-const { GH_TOKEN } = process.env;
+const { GH_TOKEN = "3f20ca63a456fc0c2dcf1196847edf78b77ac75f" } = process.env;
 const { boy, girl, gallery, gist_id } = require('./src/assets/setting.json');
 const client = create({
     headers: {
         'Authorization': `token ${GH_TOKEN}`,
-        'User-Agent': 'https://i.instagram.com/api/v1/users/2217696854/info/',
+        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)',
     }
 });
 
